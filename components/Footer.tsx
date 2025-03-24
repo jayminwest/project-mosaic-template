0b86035b7982cb8a37f7e6d339a647cde8cb15bc
jayminwest <jayminwest@gmail.com>
2025-03-24 15:21:51 -0700

    improved page and components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,15 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-transparent text-white py-4">
       <div className="container mx-auto px-4 text-center text-sm">
-        {productConfig?.name || "Project Mosaic"} - Part of Project Mosaic 🧩 View on{" "}
-        <a
-          href="https://github.com/pixegami/project-mosaic"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          GitHub
-        </a>
+        {productConfig?.name || "Project Mosaic"} - Part of Project Mosaic 🧩
       </div>
     </footer>
   );
